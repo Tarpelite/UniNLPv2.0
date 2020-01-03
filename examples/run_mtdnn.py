c4934823a32db6d0b931c79d92b3790802967ca3
Tarpelite <tarpelite_chan@foxmail.com>
2020-01-03 16:44:05 +0800

    debug

--- examples/run_mtdnn.py
+++ examples/run_mtdnn.py
@@ -312,7 +312,7 @@ def main():
                         datefmt="%m/%d/%Y %H:%M:%S",
                         level=logging.INFO )
     logger.warning("Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
-                   args.local_rank, device, args.n_gpu, bool(args.local_rank != -1), args.fp16)
+                   args.local_rank, device, args.n_gpu, False , args.fp16)
 
     # Set seed
     set_seed(args)
