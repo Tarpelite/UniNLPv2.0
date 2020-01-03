d04bf178b886ba8e56f2d28f5782888168e04177
Tarpelite <tarpelite_chan@foxmail.com>
2020-01-03 16:38:27 +0800

    debug

--- examples/run_mtdnn.py
+++ examples/run_mtdnn.py
@@ -310,7 +310,7 @@ def main():
     # Setup logging
     logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
                         datefmt="%m/%d/%Y %H:%M:%S",
-                        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+                        level=logging.INFO )
     logger.warning("Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
                    args.local_rank, device, args.n_gpu, bool(args.local_rank != -1), args.fp16)
 
