988ecd583e59b80812d76d2d8412a76fad374866
Tarpelite <tarpelite_chan@foxmail.com>
2020-01-04 15:22:12 +0800

    test multi-processing

--- examples/utils_mtdnn.py
+++ examples/utils_mtdnn.py
@@ -7,6 +7,7 @@ import yaml
 from tqdm import *
 import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
+from multiprocessing import cpu_count, Pool
 
 logger = logging.getLogger(__name__)
 
@@ -62,11 +63,75 @@ class MegaDataSet(object):
 
         return examples
 
+    def solve(self, instance):
+        task_name, label_map, example = instance
+        # label_map = {label:i for i, label in enumerate(labels)}
+        tokens = []
+        label_ids = []
+        
+        words = example[0]
+        labels = example[1]
+        if task_name == "SRL":
+            verb = words[0]
+            words = words[1:]
+
+
+        assert len(words) == len(labels)
+        for word, label in zip(words, labels):
+            word_tokens = self.tokenizer.tokenize(word)
+            tokens.extend(word_tokens)
+            label_ids.extend([label_map[label]] + [-100]*(len(word_tokens) - 1))
+
+        # cnt_counts.append(len(tokens))
+        if task_name == "SRL":
+            verb_tokens = self.tokenizer.tokenize(verb)
+            special_tokens_count = 3
+            if len(tokens) > self.max_seq_length - special_tokens_count - len(verb_tokens):
+                tokens = tokens[:(self.max_seq_length - special_tokens_count - len(verb_tokens))]
+                label_ids = label_ids[:(self.max_seq_length - special_tokens_count - len(verb_tokens))]
+        else:
+            special_tokens_count = 2
+            if len(tokens) > self.max_seq_length - special_tokens_count:
+                tokens = tokens[:(self.max_seq_length - special_tokens_count)]
+                label_ids = label_ids[:(self.max_seq_length - special_tokens_count)]
+        
+        tokens += ['[SEP]']
+        label_ids += [-100]
+        segment_ids = [0]*len(tokens)
+
+        tokens = ['[CLS]'] + tokens
+        label_ids = [-100] + label_ids
+        segment_ids = [0] + segment_ids
+
+        if task_name == "SRL":
+            tokens +=   verb_tokens + ['[SEP]']
+            label_ids +=   [-100]*(len(verb_tokens) + 1) 
+            segment_ids +=    [1]*(len(verb_tokens) + 1)
+        
+        input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+        input_mask = [1]*len(input_ids)
+
+        padding_length = self.max_seq_length - len(input_ids)
+
+        input_ids += [0]*padding_length
+        input_mask += [0]*padding_length
+        segment_ids += [0]*padding_length
+        label_ids += [-100]*padding_length
+
+        assert len(input_ids) == self.max_seq_length
+        assert len(input_mask) == self.max_seq_length
+        assert len(segment_ids) == self.max_seq_length
+        assert len(label_ids) == self.max_seq_length
+
+        return [input_ids, input_mask, segment_ids, label_ids]
+
+
+
     def load_single_dataset(self, task_name, mode):
         task = task_name.upper()
         task_id = self.task_map[task_name]
         file_path = os.path.join(self.datasets_dir, task_name, "{}.txt".format(mode))
-        print("Loading trainset from task %s".format(task))
+        print("Loading trainset from task {}".format(task))
         
         examples = self.load_examples_from_file(file_path, task_name)
         labels = self.labels_list[task_id]
@@ -79,77 +144,81 @@ class MegaDataSet(object):
         if "{}-{}".format(task, mode) in self.features_map:
             features = self.features_map["{}-{}".format(task, mode)]
         else:
-            for (ex_index, example) in enumerate(tqdm(examples)):
-                if ex_index % 10000 == 0:
-                    logger.info("Writing example %d of %d", ex_index, len(examples))
-                tokens = []
-                label_ids = []
+            # for (ex_index, example) in enumerate(tqdm(examples)):
+            #     # if ex_index % 10000 == 0:
+            #     #     logger.info("Writing example %d of %d", ex_index, len(examples))
+            #     tokens = []
+            #     label_ids = []
                 
-                words = example[0]
-                labels = example[1]
-                if task_name == "SRL":
-                    verb = words[0]
-                    words = words[1:]
+            #     words = example[0]
+            #     labels = example[1]
+            #     if task_name == "SRL":
+            #         verb = words[0]
+            #         words = words[1:]
 
 
-                assert len(words) == len(labels)
-                for word, label in zip(words, labels):
-                    word_tokens = self.tokenizer.tokenize(word)
-                    tokens.extend(word_tokens)
-                    label_ids.extend([label_map[label]] + [-100]*(len(word_tokens) - 1))
+            #     assert len(words) == len(labels)
+            #     for word, label in zip(words, labels):
+            #         word_tokens = self.tokenizer.tokenize(word)
+            #         tokens.extend(word_tokens)
+            #         label_ids.extend([label_map[label]] + [-100]*(len(word_tokens) - 1))
 
-                cnt_counts.append(len(tokens))
-                if task_name == "SRL":
-                    verb_tokens = self.tokenizer.tokenize(verb)
-                    special_tokens_count = 3
-                    if len(tokens) > self.max_seq_length - special_tokens_count - len(verb_tokens):
-                        tokens = tokens[:(self.max_seq_length - special_tokens_count - len(verb_tokens))]
-                        label_ids = label_ids[:(self.max_seq_length - special_tokens_count - len(verb_tokens))]
-                else:
-                    special_tokens_count = 2
-                    if len(tokens) > self.max_seq_length - special_tokens_count:
-                        tokens = tokens[:(self.max_seq_length - special_tokens_count)]
-                        label_ids = label_ids[:(self.max_seq_length - special_tokens_count)]
+            #     cnt_counts.append(len(tokens))
+            #     if task_name == "SRL":
+            #         verb_tokens = self.tokenizer.tokenize(verb)
+            #         special_tokens_count = 3
+            #         if len(tokens) > self.max_seq_length - special_tokens_count - len(verb_tokens):
+            #             tokens = tokens[:(self.max_seq_length - special_tokens_count - len(verb_tokens))]
+            #             label_ids = label_ids[:(self.max_seq_length - special_tokens_count - len(verb_tokens))]
+            #     else:
+            #         special_tokens_count = 2
+            #         if len(tokens) > self.max_seq_length - special_tokens_count:
+            #             tokens = tokens[:(self.max_seq_length - special_tokens_count)]
+            #             label_ids = label_ids[:(self.max_seq_length - special_tokens_count)]
                 
-                tokens += ['[SEP]']
-                label_ids += [-100]
-                segment_ids = [0]*len(tokens)
+            #     tokens += ['[SEP]']
+            #     label_ids += [-100]
+            #     segment_ids = [0]*len(tokens)
 
-                tokens = ['[CLS]'] + tokens
-                label_ids = [-100] + label_ids
-                segment_ids = [0] + segment_ids
+            #     tokens = ['[CLS]'] + tokens
+            #     label_ids = [-100] + label_ids
+            #     segment_ids = [0] + segment_ids
 
-                if task_name == "SRL":
-                    tokens +=   verb_tokens + ['[SEP]']
-                    label_ids +=   [-100]*(len(verb_tokens) + 1) 
-                    segment_ids +=    [1]*(len(verb_tokens) + 1)
+            #     if task_name == "SRL":
+            #         tokens +=   verb_tokens + ['[SEP]']
+            #         label_ids +=   [-100]*(len(verb_tokens) + 1) 
+            #         segment_ids +=    [1]*(len(verb_tokens) + 1)
                 
-                input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
-                input_mask = [1]*len(input_ids)
+            #     input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+            #     input_mask = [1]*len(input_ids)
 
-                padding_length = self.max_seq_length - len(input_ids)
+            #     padding_length = self.max_seq_length - len(input_ids)
 
-                input_ids += [0]*padding_length
-                input_mask += [0]*padding_length
-                segment_ids += [0]*padding_length
-                label_ids += [-100]*padding_length
+            #     input_ids += [0]*padding_length
+            #     input_mask += [0]*padding_length
+            #     segment_ids += [0]*padding_length
+            #     label_ids += [-100]*padding_length
 
-                assert len(input_ids) == self.max_seq_length
-                assert len(input_mask) == self.max_seq_length
-                assert len(segment_ids) == self.max_seq_length
-                assert len(label_ids) == self.max_seq_length
+            #     assert len(input_ids) == self.max_seq_length
+            #     assert len(input_mask) == self.max_seq_length
+            #     assert len(segment_ids) == self.max_seq_length
+            #     assert len(label_ids) == self.max_seq_length
 
-                if ex_index < 5:
-                    logger.info("*** {} Example ***".format(task))
-                    logger.info("tokens: %s", " ".join([str(x) for x in tokens]))
-                    logger.info("input_ids: %s", " ".join([str(x) for x in input_ids]))
-                    logger.info("input_mask: %s", " ".join([str(x) for x in input_mask]))
-                    logger.info("segment_ids: %s", " ".join([str(x) for x in segment_ids]))
-                    logger.info("label_ids: %s", " ".join([str(x) for x in label_ids]))
+            #     if ex_index < 5:
+            #         logger.info("*** {} Example ***".format(task))
+            #         logger.info("tokens: %s", " ".join([str(x) for x in tokens]))
+            #         logger.info("input_ids: %s", " ".join([str(x) for x in input_ids]))
+            #         logger.info("input_mask: %s", " ".join([str(x) for x in input_mask]))
+            #         logger.info("segment_ids: %s", " ".join([str(x) for x in segment_ids]))
+            #         logger.info("label_ids: %s", " ".join([str(x) for x in label_ids]))
 
-                features.append([
-                    input_ids, input_mask, segment_ids, label_ids
-                ])
+            #     features.append([
+            #         input_ids, input_mask, segment_ids, label_ids
+            #     ])
+
+            instances = [[task_name, label_map, example] for example in examples]
+            with Pool(cpu_count()) as p:
+                features = list(tqdm(p.imap(self.solve, instances), total=len(instances)))
             
 
             self.features_map["{}-{}".format(task, "train")] = features
