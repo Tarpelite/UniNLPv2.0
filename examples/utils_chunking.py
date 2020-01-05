1a9c21cdfc1ee8520bb4162489ea27a2a2ad7e40
Tarpelite <tarpelite_chan@foxmail.com>
2020-01-05 15:46:26 +0800

    debug

--- examples/utils_chunking.py
+++ examples/utils_chunking.py
@@ -116,15 +116,15 @@ def convert_examples_to_features(examples,
             # Use the real label id for the first token of the word, and padding ids for the remaining tokens
             label_ids.extend([label_map[label]] + [pad_token_label_id] * (len(word_tokens) - 1))
 
-        if ex_index == 0:
-            last_tokens = tokens[-64:]
-            last_label_ids = label_ids[-64:]
+        # if ex_index == 0:
+        #     last_tokens = tokens[-64:]
+        #     last_label_ids = label_ids[-64:]
         
-        else:
-            tokens = last_tokens + tokens
-            label_ids = last_label_ids + label_ids
-            last_tokens= tokens[-64:]
-            last_label_ids = label_ids[-64:]
+        # else:
+        #     tokens = last_tokens + tokens
+        #     label_ids = last_label_ids + label_ids
+        #     last_tokens= tokens[-64:]
+        #     last_label_ids = label_ids[-64:]
 
         # Account for [CLS] and [SEP] with "- 2" and with "- 3" for RoBERTa.
         cnt_counts.append(len(tokens))
