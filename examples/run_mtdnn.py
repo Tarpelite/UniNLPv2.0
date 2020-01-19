bd0419598435e371edbd13bdc3e353ecd680a19c
Tarpelite <tarpelite_chan@foxmail.com>
2020-01-19 14:50:45 +0800

    debug

--- examples/run_mtdnn.py
+++ examples/run_mtdnn.py
@@ -143,7 +143,7 @@ def train(args, model, datasets, all_dataset_sampler, task_id=-1):
             train_dataloader = DataLoader(datasets, sampler=train_sampler, batch_size=args.train_batch_size)
         else:
             train_dataloader = DataLoader(datasets, sampler=all_dataset_sampler)
-        epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=False)
+        # epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=False)
         model.train()
         iter_bar = tqdm(train_dataloader, desc="Iter(loss=X.XXX)")
         for step, batch in enumerate(iter_bar):
@@ -213,7 +213,7 @@ def train(args, model, datasets, all_dataset_sampler, task_id=-1):
                     logger.info("Saving model checkpoint to %s", output_dir)
 
             if args.max_steps > 0 and global_step > args.max_steps:
-                epoch_iterator.close()
+                iter_bar.close()
                 break
         if args.max_steps > 0 and global_step > args.max_steps:
             train_iterator.close()
