c3eaf711356f44ffda0d5925d00cbc996953611f
Tarpelite <tarpelite_chan@foxmail.com>
2020-02-16 10:34:08 +0800

    debug

--- uninlp/modeling_decoder.py
+++ uninlp/modeling_decoder.py
@@ -233,9 +233,9 @@ class uninlp(object):
     
     def batchfy_predict(self, input_text, task, verb=None, max_seq_length=128, batch_size=32):
         sentences = self.sent_tokenizer.tokenize(input_text)
-        print(sentences)
-        max_len = max([len(sent.split()) for sent in sentences])
-        max_seq_length = max_seq_length * max(1, int((max_len+1)/ 32))
+        # print(sentences)
+        # max_len = max([len(sent.split()) for sent in sentences])
+        # max_seq_length = max_seq_length * max(1, int((max_len+1)/ 32))
         all_input_ids = []
         all_input_mask = []
         all_segment_ids = []
