d962d1caf42f4186a46969ef071d45378bb0d8e4
Tarpelite <tarpelite_chan@foxmail.com>
2020-03-19 17:40:43 +0800

    debug

--- uninlp/modeling_bert.py
+++ uninlp/modeling_bert.py
@@ -1980,7 +1980,7 @@ class MTDNNModelMobile(BertPreTrainedModel):
         self.task_list = task_list
         self.init_weights()
 
-    def forward(self, tup:List[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]):
+    def forward(self, tup:List[torch.Tensor]):
                
         input_ids, attention_mask, token_type_ids, task_id = tup
         outputs = self.bert(input_ids, 
