2f97413ab209391a79800c59239187b2abb006db
Tarpelite <tarpelite_chan@foxmail.com>
2020-03-19 14:44:46 +0800

    debug

--- examples/run_ner_pos.py
+++ examples/run_ner_pos.py
@@ -772,7 +772,8 @@ def main():
     # Evaluation
     results = {}
     if args.do_eval and args.local_rank in [-1, 0]:
-        tokenizer = tokenizer_class.from_pretrained(args.output_dir, **tokenizer_args)
+        tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
+        cache_dir=args.cache_dir if args.cache_dir else None, do_lower_case=args.do_lower_case)
         checkpoints = [args.output_dir]
         if args.eval_all_checkpoints:
             checkpoints = list(
